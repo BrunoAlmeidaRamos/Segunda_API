02ab54373f431d5bd65be2213e8c2c352a08432a
Bruno Ramos <robrumi01@gmail.com>
2025-07-17 13:43:06 -0300

    Fazendo as Tabelas

---
+++ Segunda_API/Segunda_API/Context/AddDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Segunda_API.Models;
+
+namespace Segunda_API.Context;
+
+public class AddDbContext
+{
+    public AddDbContext(DbContextOptions<AppContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria>? Categorias {set; get;}
+    public DbSet<Produto>? Produtos { set; get; }
+}   
+
---
+++ Segunda_API/Segunda_API/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace Segunda_API.Models;
+
+public class Produto
+{
+    public int ProdutoId {set; get;}
+    public string? Nome {set; get;}
+    public string? Descricao {set; get;}
+    public decimal Preco {set; get;}
+    public string? ImagemUrl {set; get;}
+    public float Estoque {set; get;}
+    public DateTime DataCadastro {set; get;}
+    public int CategoriaId {set; get;}
+
+}

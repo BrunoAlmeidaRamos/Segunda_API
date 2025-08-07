692b83b581177c5042f4c25954aeed898a54ee6b
Bruno Ramos <robrumi01@gmail.com>
2025-08-07 16:50:43 -0300

    Fazendo repositório específicos para categorias

---
+++ Segunda_API/Segunda_API/Repositories/CategoriaRepository.cs
@@ -0,0 +1,34 @@
+﻿using Segunda_API.Context;
+using Segunda_API.Models;
+
+namespace Segunda_API.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    private readonly AppContext _context;
+    public CategoriaRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IEnumerable<Categoria> GeCategorias()
+    {
+        throw new NotImplementedException();
+    }
+    public Categoria GetCategoria(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public Categoria Create(Categoria categoria)
+    {
+        throw new NotImplementedException();
+    }
+    public Categoria Update(Categoria categoria)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Categoria Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Segunda_API/Segunda_API/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Segunda_API.Models;
+
+namespace Segunda_API.Repositories;
+
+public interface ICategoriaRepository
+{
+    IEnumerable<Categoria> GeCategorias();
+    Categoria GetCategoria(int id);
+    Categoria Create (Categoria categoria);
+    Categoria Update(Categoria categoria);
+    Categoria Delete(int id);
+}

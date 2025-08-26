7d92408123205714c9a13887cf7b11deeebd5aef
Bruno Ramos <robrumi01@gmail.com>
2025-08-26 12:56:32 -0300

    Extensão

---
+++ Segunda_API/Segunda_API/DTOs/Mappings/CategoriaDTOMappingExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Segunda_API.Models;
+
+namespace Segunda_API.DTOs.Mappings;
+
+public static class CategoriaDTOMappingExtensions
+{
+    public static CategoriaDTO? ToCategoriaDTO(this Categoria categoria)
+    {         
+        if (categoria == null) 
+            return null;
+
+        return new CategoriaDTO
+        {
+            CategoriaId = categoria.CategoriaId,
+            Nome = categoria.Nome,
+            ImagemUrl = categoria.ImagemUrl
+        };
+    }
+
+    public static Categoria? ToCategoria(this CategoriaDTO categoriaDto)
+    {
+        if (categoriaDto == null) 
+            return null;
+
+        return new Categoria
+        {
+            CategoriaId = categoriaDto.CategoriaId,
+            Nome = categoriaDto.Nome,
+            ImagemUrl = categoriaDto.ImagemUrl
+        };
+    }
+
+    public static IEnumerable<CategoriaDTO> ToCategoriaDTOList(this IEnumerable<Categoria> categorias)
+    {
+        if (categorias == null) 
+            return Enumerable.Empty<CategoriaDTO>();
+        return categorias.Select(categoria => categoria.ToCategoriaDTO()!).ToList();
+    }
+}

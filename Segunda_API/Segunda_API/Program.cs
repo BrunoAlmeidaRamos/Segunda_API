eaed5f8c37992c9d26fce590f81d246a6f6d1360
Bruno Ramos <robrumi01@gmail.com>
2025-09-01 16:33:06 -0300

    instalando os pacotes do jsonPatch

--- Segunda_API/Segunda_API/Program.cs
+++ Segunda_API/Segunda_API/Program.cs
@@ -9,8 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles).AddNewtonsoftJson();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
